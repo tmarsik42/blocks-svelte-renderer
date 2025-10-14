6d522465149ee5c2e459378795b6dad97fabc61e
tmarsik42 <tad.marsik@gmail.com>
2025-10-14 13:13:05 +0200

    fix: Correct quote rendering

--- src/lib/components/blocks/Quote.svelte
+++ src/lib/components/blocks/Quote.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import BlocksRenderer from '../BlocksRenderer.svelte';
+    import InlineRenderer from '../InlineRenderer.svelte';
     import type { QuoteProps } from '$lib/types/index.js';
 
     const { node, modifiers }: QuoteProps = $props();
 </script>
 
 <blockquote>
-    <BlocksRenderer content={node.children} blocks={{}} {modifiers} />
+    <InlineRenderer nodes={node.children} {modifiers} />
 </blockquote>
