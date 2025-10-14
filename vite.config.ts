00a637e9058dd5b7b2c9c99962e2abc5fe0e63a8
tmarsik42 <tad.marsik@gmail.com>
2025-10-14 14:47:15 +0200

    ci: Fix tests

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
             enabled: true,
             provider: 'playwright',
             instances: [{ browser: 'chromium' }],
+            headless: true,
         },
     },
 });
--- vitest.browser.config.ts
+++ vitest.browser.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vitest/config'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [svelte()],
-  test: {
-    browser: {
-      enabled: true,
-      provider: 'playwright',
-      // https://vitest.dev/guide/browser/playwright
-      instances: [
-      { browser: 'chromium' },
-      ],
+    plugins: [svelte()],
+    test: {
+        browser: {
+            enabled: true,
+            provider: 'playwright',
+            // https://vitest.dev/guide/browser/playwright
+            instances: [{ browser: 'chromium' }],
+        },
     },
-  },
-})
+    resolve: {
+        alias: {
+            $lib: path.resolve('./src/lib'),
+        },
+    },
+});
