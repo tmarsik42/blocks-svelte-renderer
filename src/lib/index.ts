77dda87b74e53f56f03403ee230e7dc94f732e12
tmarsik42 <tad.marsik@gmail.com>
2025-10-15 12:26:45 +0200

    ci: Run formatting before making release PR

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,6 @@
 export { default as BlocksRenderer } from '$lib/components/BlocksRenderer.svelte';
+export { default as InlineRenderer } from '$lib/components/InlineRenderer.svelte';
+export { default as ModifierWrapper } from '$lib/components/ModifierWrapper.svelte';
+export { default as TextRenderer } from '$lib/components/TextRenderer.svelte';
 
 export * from '$lib/types/index.js';
